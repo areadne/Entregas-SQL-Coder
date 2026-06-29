a64bbf8ba96911587c79607c8bbc9215b0f982c6
areadne <115907950+areadne@users.noreply.github.com>
2026-06-29 16:33:42 -0400

    Update m4_consultas_negocio.sql

--- m4_consultas_negocio.sql
+++ m4_consultas_negocio.sql
@@ -1,23 +1,20 @@
 -- Consulta 1 — Resumen ejecutivo mensual Total facturado, cantidad de pedidos y ticket promedio, agrupados por mes. 
 -- Calculá el total como cantidad * precio_unitario. Usá alias descriptivos en español y agrupá por mes con EXTRACT(MONTH FROM fecha_venta).
 
-SELECT * FROM ventas
-
 SELECT 
-	MONTH(fecha_venta) AS Mes,
-	SUM(cantidad * precio_unitario) as Total_facturado,
-	COUNT(id_venta) as Cantidad_de_pedidos,
-	AVG(precio_unitario * cantidad) as Ticket_promedio
+    EXTRACT(MONTH FROM fecha_venta) AS Mes,
+    SUM(cantidad * precio_unitario) AS Total_facturado,
+    COUNT(id_venta) AS Cantidad_de_pedidos,
+    SUM(cantidad * precio_unitario) / COUNT(id_venta) AS Ticket_promedio
 FROM 
-	ventas
+    ventas
 GROUP BY 
-	MONTH(fecha_venta);
-
+    EXTRACT(MONTH FROM fecha_venta);
 
 -- Consulta 2 — Ranking de productos Top 5 de id_producto por total facturado, mostrando las unidades vendidas (SUM(cantidad)) 
 -- y el total generado. Usá GROUP BY id_producto, ORDER BY y limitá el resultado a 5.
 
-SELECT TOP 5 
+SELECT
 	id_producto, 
 	SUM(cantidad) as Total_cantidad, 
 	SUM(precio_unitario) as Total_compra 
@@ -27,11 +24,10 @@ GROUP BY
 	id_producto
 ORDER BY 
 	Total_cantidad DESC
+LIMIT 5;
 
 -- Consulta 3 — Clientes recurrentes id_cliente que hayan realizado más de un pedido, mostrando la cantidad de pedidos 
 -- y el total gastado. Usá GROUP BY id_cliente y HAVING COUNT(*) > 1.
-
-SELECT * FROM ventas;
 
 SELECT 
 	id_cliente, 
@@ -64,3 +60,10 @@ FROM
 	ventas
 GROUP BY 
 	MONTH(fecha_venta);
+
+-- Hallazgos
+-- 1. Cada cliente ha hecho al menos dos compras. Son clientes recurrentes. Se podrian obtener sus datos de correos y correr campañas de marketing con productos para incentivar la compra.
+
+-- 2. El mouse inalambrico es el producto con más compras en el mes 3.
+
+-- 3. El total facturado el mes 3 está por encimad el ticket promedio, sin embargo, falta información de más meses para hacer un análisis más robusto.
